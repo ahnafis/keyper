eead9a12a01a61a5e898058aa445b0d321754c3b
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2025-07-29 16:06:12 +0600

    chore(tests): Refactor tests for input()
    Renamed the cin_redirect class to input_stream and moved the method
    declarations inside the class definition;

--- tests/input.test.cc
+++ tests/input.test.cc
@@ -6,26 +6,23 @@
 #include <sstream>
 #include <string>
 
-class cin_redirect {
+class input_stream {
   public:
-    explicit cin_redirect(const std::istringstream& input_stream);
-    ~cin_redirect();
+    explicit input_stream(const std::istringstream& input_stream) {
+        this->old_buffer = std::cin.rdbuf(input_stream.rdbuf());
+    }
+
+    ~input_stream() {
+        std::cin.rdbuf(old_buffer);
+    }
 
   private:
     std::streambuf* old_buffer;
 };
 
-cin_redirect::cin_redirect(const std::istringstream& input_stream) {
-    this->old_buffer = std::cin.rdbuf(input_stream.rdbuf());
-}
-
-cin_redirect::~cin_redirect() {
-    std::cin.rdbuf(old_buffer);
-}
-
 TEST_CASE("Should convert command-line input to int") {
     std::istringstream fake_input("42\n");
-    cin_redirect redirect(fake_input);
+    const auto copy = input_stream(fake_input);
 
     CHECK_EQ(std::stoi(input()), 42);
 }
@@ -34,7 +31,7 @@ TEST_CASE("Should convert command-line input to string") {
     std::string text = "C++ is the best";
 
     std::istringstream fake_input(text + '\n');
-    cin_redirect redirect(fake_input);
+    const auto copy = input_stream(fake_input);
 
     CHECK_EQ(input(), text);
 }
